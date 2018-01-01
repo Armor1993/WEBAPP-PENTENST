20acd8508afd6e723d648d74d1fe8601ee020c8f
raafeh <raafehmehmood@ymail.com>
2018-01-01 17:29:50 +0300

    added a test function

--- zaproxy.py
+++ zaproxy.py
@@ -1,5 +1,6 @@
 import time
 from datetime import datetime
+from pprint import pprint
 from zapv2 import ZAPv2
 from app import db, Process, Target, Scan
 
@@ -64,40 +65,43 @@ class Zap:
         proc.progress = 100
         db.session.commit()
 
-# # By default ZAP API client will connect to port 8080
-# zap = ZAPv2(apikey=None)
-# # Use the line below if ZAP is not listening on port 8080, for example, if listening on port 8090
-# # zap = ZAPv2(apikey=apikey, proxies={'http': 'http://127.0.0.1:8090', 'https': 'http://127.0.0.1:8090'})
-#
-# # do stuff
-# print('Accessing target %s' % target)
-# # try have a unique enough session...
-# zap.urlopen(target)
-# # Give the sites tree a chance to get updated
-# time.sleep(10)
-#
-# print('Spidering target %s' % target)
-# scanid = zap.spider.scan(target)
-# # Give the Spider a chance to start
-# time.sleep(10)
-# while int(zap.spider.status(scanid)) < 100:
-#     print('Spider progress %: ' + zap.spider.status(scanid))
-#     time.sleep(5)
-#
-# print('Spider completed')
-# # Give the passive scanner a chance to finish
-# time.sleep(5)
 
-# print('Scanning target %s' % target)
-# scanid = zap.ascan.scan(target)
-# while int(zap.ascan.status(scanid)) < 100:
-#     print('Scan progress %: ' + zap.ascan.status(scanid))
-#     time.sleep(5)
-#
-# print('Scan completed')
+def test():
+    # By default ZAP API client will connect to port 8080
+    zap = ZAPv2(apikey=None)
+    target = 'http://setloki.com'
+    # Use the line below if ZAP is not listening on port 8080, for example, if listening on port 8090
+    # zap = ZAPv2(apikey=apikey, proxies={'http': 'http://127.0.0.1:8090', 'https': 'http://127.0.0.1:8090'})
 
-# Report the results
+    # do stuff
+    print('Accessing target %s' % target)
+    # try have a unique enough session...
+    zap.urlopen(target)
+    # Give the sites tree a chance to get updated
+    time.sleep(10)
 
-# print('Hosts: ' + ', '.join(zap.core.hosts))
-# print('Alerts: ')
-# pprint(zap.core.alerts())
+    print('Spidering target %s' % target)
+    scanid = zap.spider.scan(target)
+    # Give the Spider a chance to start
+    time.sleep(10)
+    while int(zap.spider.status(scanid)) < 100:
+        print('Spider progress %: ' + zap.spider.status(scanid))
+        time.sleep(5)
+
+    print('Spider completed')
+    # Give the passive scanner a chance to finish
+    time.sleep(5)
+
+    print('Scanning target %s' % target)
+    scanid = zap.ascan.scan(target)
+    while int(zap.ascan.status(scanid)) < 100:
+        print('Scan progress %: ' + zap.ascan.status(scanid))
+        time.sleep(5)
+
+    print('Scan completed')
+
+    #Report the results
+
+    print('Hosts: ' + ', '.join(zap.core.hosts))
+    print('Alerts: ')
+    pprint(zap.core.alerts())
